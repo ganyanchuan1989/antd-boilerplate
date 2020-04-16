04de5238867099730aedc8f727cb0c520dcaebf6
ganxunzou <ganxunzou@163.com>
2020-04-16 14:32:48 +0800

    demo

--- src/views/jsFiddle/demo.js
+++ src/views/jsFiddle/demo.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
 import { Row, Col, Button, Form, Input, Checkbox, Radio, Select } from 'antd';
-
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 6 },
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 12 },
-  },
-};
+import styles from '../index.less';
 
 class JSFiddleDemo extends Component {
   handleSubmit = (e) => {
@@ -31,11 +21,11 @@ class JSFiddleDemo extends Component {
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
-      <div>
-        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+      <div className={styles.FormContainer}>
+        <Form onSubmit={this.handleSubmit}>
           <Row>
-            <Col span={24}>
-              <Form.Item label="姓名">
+            <Col span={8}>
+              <Form.Item label="姓名" className={styles.FormItem}>
                 {getFieldDecorator('Field1', {
                   rules: [
                     {
@@ -45,8 +35,8 @@ class JSFiddleDemo extends Component {
                 })(<Input type="input" placeholder="请输入姓名" />)}
               </Form.Item>
             </Col>
-            <Col span={24}>
-              <Form.Item label="密码">
+            <Col span={8}>
+              <Form.Item label="密码" className={styles.FormItem}>
                 {getFieldDecorator('Field2', {
                   rules: [
                     {
@@ -56,9 +46,20 @@ class JSFiddleDemo extends Component {
                 })(<Input type="password" placeholder="请输入密码" />)}
               </Form.Item>
             </Col>
-            <Col span={24}>
-              <Form.Item label="爱好">
+            <Col span={8}>
+              <Form.Item label="密码" className={styles.FormItem}>
                 {getFieldDecorator('Field3', {
+                  rules: [
+                    {
+                      required: true,
+                    },
+                  ],
+                })(<Input type="password" placeholder="请输入密码" />)}
+              </Form.Item>
+            </Col>
+            <Col span={24}>
+              <Form.Item label="爱好" className={styles.FormItem}>
+                {getFieldDecorator('Field4', {
                   rules: [
                     {
                       required: false,
@@ -66,22 +67,39 @@ class JSFiddleDemo extends Component {
                   ],
                 })(
                   <Checkbox.Group style={{ width: '100%' }}>
-                    <Row>
-                      <Col span={6}>
-                        <Checkbox value="羽毛球">羽毛球</Checkbox>
-                      </Col>
-                      <Col span={6}>
-                        <Checkbox value="乒乓球">乒乓球</Checkbox>
-                      </Col>
-                    </Row>
+                    <div style={{ display: 'flex' }}>
+                      <Checkbox value="羽毛球">羽毛球</Checkbox>
+                      <Checkbox value="乒乓球">乒乓球</Checkbox>
+                      <Checkbox value="篮球">篮球</Checkbox>
+                    </div>
                   </Checkbox.Group>
                 )}
               </Form.Item>
             </Col>
 
             <Col span={24}>
-              <Form.Item label="性别">
-                {getFieldDecorator('Field4', {
+              <Form.Item label="爱好" className={styles.FormItem}>
+                {getFieldDecorator('Field5', {
+                  rules: [
+                    {
+                      required: false,
+                    },
+                  ],
+                })(
+                  <Checkbox.Group style={{ width: '100%' }}>
+                    <div style={{ display: 'flex' }}>
+                      <Checkbox value="羽毛球">羽毛球</Checkbox>
+                      <Checkbox value="乒乓球">乒乓球</Checkbox>
+                      <Checkbox value="篮球">篮球</Checkbox>
+                    </div>
+                  </Checkbox.Group>
+                )}
+              </Form.Item>
+            </Col>
+
+            <Col span={24}>
+              <Form.Item label="性别" className={styles.FormItem}>
+                {getFieldDecorator('Field6', {
                   rules: [
                     {
                       required: false,
@@ -97,8 +115,8 @@ class JSFiddleDemo extends Component {
             </Col>
 
             <Col span={24}>
-              <Form.Item label="好友">
-                {getFieldDecorator('Field5', {
+              <Form.Item label="好友" className={styles.FormItem}>
+                {getFieldDecorator('Field7', {
                   rules: [
                     {
                       required: true,
@@ -113,9 +131,43 @@ class JSFiddleDemo extends Component {
               </Form.Item>
             </Col>
 
-            <Col span={24}>
-              <Form.Item label="好友">
-                {getFieldDecorator('Field6', {
+            <Col span={8}>
+              <Form.Item label="好友" className={styles.FormItem}>
+                {getFieldDecorator('Field8', {
+                  rules: [
+                    {
+                      required: false,
+                    },
+                  ],
+                })(
+                  <Select placeholder="请选择好友">
+                    <Select.Option value="lili">lili</Select.Option>
+                    <Select.Option value="lucy">lucy</Select.Option>
+                  </Select>
+                )}
+              </Form.Item>
+            </Col>
+
+            <Col span={8}>
+              <Form.Item label="好友" className={styles.FormItem}>
+                {getFieldDecorator('Field9', {
+                  rules: [
+                    {
+                      required: false,
+                    },
+                  ],
+                })(
+                  <Select placeholder="请选择好友">
+                    <Select.Option value="lili">lili</Select.Option>
+                    <Select.Option value="lucy">lucy</Select.Option>
+                  </Select>
+                )}
+              </Form.Item>
+            </Col>
+
+            <Col span={8}>
+              <Form.Item label="好友" className={styles.FormItem}>
+                {getFieldDecorator('Field10', {
                   rules: [
                     {
                       required: false,
@@ -131,8 +183,8 @@ class JSFiddleDemo extends Component {
             </Col>
 
             <Col span={24}>
-              <Form.Item label="简介">
-                {getFieldDecorator('Field7', {
+              <Form.Item label="简介" className={styles.FormItem}>
+                {getFieldDecorator('Field11', {
                   rules: [
                     {
                       required: true,
@@ -142,30 +194,8 @@ class JSFiddleDemo extends Component {
               </Form.Item>
             </Col>
             <Col span={24}>
-              <Form.Item label="简介">
-                {getFieldDecorator('Field8', {
-                  rules: [
-                    {
-                      required: true,
-                    },
-                  ],
-                })(<Input.TextArea placeholder="请输入简介" />)}
-              </Form.Item>
-            </Col>
-            <Col span={24}>
-              <Form.Item label="简介">
-                {getFieldDecorator('Field9', {
-                  rules: [
-                    {
-                      required: true,
-                    },
-                  ],
-                })(<Input.TextArea placeholder="请输入简介" />)}
-              </Form.Item>
-            </Col>
-            <Col span={24}>
-              <Form.Item label="简介">
-                {getFieldDecorator('Field10', {
+              <Form.Item label="简介" className={styles.FormItem}>
+                {getFieldDecorator('Field12', {
                   rules: [
                     {
                       required: true,
@@ -176,7 +206,7 @@ class JSFiddleDemo extends Component {
             </Col>
           </Row>
           <Row>
-            <Form.Item label=" " colon={false}>
+            <Form.Item label=" " colon={false} className={styles.FormItem}>
               <Button
                 type="primary"
                 style={{ marginRight: 8 }}
